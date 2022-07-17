28538a33443c70807a12788a594674faa724e6e1
Eduard Kudlanov <eduardokud@mail.ru>
2022-07-17 21:58:48 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int s = 1000; // стоимость билета
+        int b = s / 20;//переменная для хранения бонусов
+        System.out.println("Количество бонусных миль = " + b );
+
+    }
+}
