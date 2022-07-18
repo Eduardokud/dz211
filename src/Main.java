f4e744c74a7bfd85ea145d89402d727f9b8c0386
Eduard Kudlanov <eduardokud@mail.ru>
2022-07-19 01:43:06 +0300

    reformat commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int s = 1000; // стоимость билета
         int b = s / 20;//переменная для хранения бонусов
-        System.out.println("Количество бонусных миль = " + b );
+        System.out.println("Количество бонусных миль = " + b);
 
     }
 }
